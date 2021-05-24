d17d0a764d4df8ffb46867b0e6a5fe9b47bd0d4f
Gabriel de Paula Valentim <cerogabgio@gmail.com>
2021-05-23 23:50:57 -0300

    correcao vue.config.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use('/api/posts', posts)
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(__dirname + '/public/'));
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.js'));
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 const port = process.env.PORT || 5000;
 
